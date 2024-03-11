3247215b839132df7b9cc8f906d967d637b51336
Mello Birkan <mgbirkan@icloud.com>
2024-03-11 18:05:54 -0300

    exercicio 3 adicionado

--- main.c
+++ main.c
@@ -26,11 +26,28 @@ int compara(int r[], int j, int s[], int k) {
     return -1;
 }
 
+// 3
+bool eSubsequencia(int s[], int k, int a[], int n) {
+    int i = 0, j = 0;
+    while (i < k && j < n) {
+        if (s[i] == a[j]) {
+            i++;
+        }
+        j++;
+    }
+    if (i == k) {
+        return true;
+    }
+    return false;
+}
+
 int main() {
     int r[] = {4, 4, 4};
+    int a[] = {4, 4, 4, 5, 6, 7, 8};
     int s[] = {4, 4, 4};
 
     printf("%s\n", eMenor(r, 3, s, 3) ? "true" : "false");
-    printf("%d", compara(r, 3, s, 3));
+    printf("%d\n", compara(r, 3, s, 3));
+    printf("%s\n", eSubsequencia(a, 7, s, 3) ? "true" : "false");
     return 0;
 }
