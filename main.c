e7e8bc3ac2793308cc3e256d355d2ac66abcf190
Mello Birkan <mgbirkan@icloud.com>
2024-03-13 12:43:34 -0300

    exercicio 6 adicionado

--- main.c
+++ main.c
@@ -69,6 +69,25 @@ int quantasSubsequencias(int arr[], int n) { // Complexidade O(n)
     return cont;
 }
 
+// 6
+int comprimentoMaximo(int arr[], int n) { // Complexidade O(n^2)
+    int ptr1 = 0, count = 0, maior = 0;
+
+    while (ptr1 < n) {
+        while (arr[ptr1] < arr[ptr1 + 1]) {
+            count++;
+            ptr1++;
+        }
+        ptr1++;
+
+        if (count + 1 > maior) {
+            maior = count + 1;
+        } else {
+            count = 0;
+        }
+    }
+    return maior;
+}
 
 int main() {
     int r[] = {4, 4, 4};
@@ -77,6 +96,8 @@ int main() {
     int b[] = {4, 5, 6};
     int sub[] = {5, 2, 2, 3, 4, 4, 4, 4, 4, 1, 1};
     int sub2[] = {3, 3, 1, 1, 1, 12, 12, 12, 3, 3};
+    int segCres[] = {5, 10, 3, 2, 4, 7, 9, 8, 5};
+    int segCres2[] = {10, 8, 7, 5, 2};
 
     printf("%s\n", eMenor(r, 3, s, 3) ? "true" : "false");
     printf("%d\n", compara(r, 3, s, 3));
@@ -84,5 +105,7 @@ int main() {
     printf("%s\n", eSegmento(b, 3, a, 7) ? "true" : "false");
     printf("%d\n", quantasSubsequencias(sub, 11));
     printf("%d\n", quantasSubsequencias(sub2, 10));
+    printf("%d\n", comprimentoMaximo(segCres, 9));
+    printf("%d\n", comprimentoMaximo(segCres2, 5));
     return 0;
 }
